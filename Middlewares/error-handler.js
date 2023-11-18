20f25af2166939cdc3d1a1fc4ef7879f95d86066
houssemgam <gammoudi.houssem@esprit.tn>
2023-11-18 23:07:13 +0100

    delete

--- Middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-export function notFoundError(req, res, next) {
-    const err = new Error("Page not found !")
-    err.status = 404
-    next(err)
-}
-
-export function errorHandler(err, req, res, next) {
-    res.status(err.status || 500).send(err.message)
-}
